984f7881c030f726f3e1df3b2c624400705dc83f
Chris Pilcher <christopher.a.pilcher@gmail.com>
2017-07-05 22:24:46 -0400

    Minor tweaks to usability

--- src/week3/lab6/TestScore.java
+++ src/week3/lab6/TestScore.java
@@ -9,13 +9,13 @@ import java.util.Scanner;
 public class TestScore {
     public static void main(String[] args){
         Scanner console = new Scanner(System.in);
-        // Create the roster with 5 indexes
+        // Create the roster with 5 indexes. These represent thefive students.
         double[] roster = new double[5];
 
-        // Loop through teh roster and just try to put each new double from our scanner into their grade
+        // Loop through the roster and just try to put each new double from our scanner into their grade
         for (int i = 0; i < roster.length; i ++){
             try{
-                roster[i] = getScore(console);
+                roster[i] = getScore(console, i);
             } catch(ScoreException ex){
                 // If the score is too high or low, we don't let it through.
                 System.out.println(ex.getMessage());
@@ -27,10 +27,11 @@ public class TestScore {
             System.out.printf("Student %d:\t%.2f\n", i, roster[i]);
         }
     }
+    
     // Our getScore method will attempt to get a double from the user, and throw a ScoreException if that double is
     // not within the predetermined range.
-    private static double getScore(Scanner input) throws ScoreException{
-        System.out.println("What score did the student get?");
+    private static double getScore(Scanner input, int studentID) throws ScoreException{
+        System.out.printf("What score did Student %d student get?\n", studentID);
         double score = input.nextDouble();
 
         if (score > 100 || score < 0){
