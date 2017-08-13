e940b11434973112fb3b7db750d54785606cf84f
Chris Pilcher <flippedbauxite@gmail.com>
2017-08-13 15:27:27 -0400

    Moved week6.assignment1.Customer to its own java file

--- CIS407/src/week6/assignment1/BuyInsurance.java
+++ CIS407/src/week6/assignment1/BuyInsurance.java
@@ -1,6 +1,5 @@
 package week6.assignment1;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class BuyInsurance {
@@ -93,56 +92,4 @@ public class BuyInsurance {
     }
 }
 
-// I'm just going to use this as a place to store insurances, rather than have them floating around.
-class Customer {
-    LinkedList<TravelInsurance> travel;
-    LinkedList<AutomobileInsurance> autos;
-    LinkedList<PropertyInsurance> properties;
 
-    public Customer(){
-        this.travel = new LinkedList<TravelInsurance>();
-        this.autos = new LinkedList<AutomobileInsurance>();
-        this.properties = new LinkedList<PropertyInsurance>();
-    }
-
-    public void showInsurance(){
-        // This is a simple switch to see if we've actually displayed anything.
-        // If we haven't we'll display a message ot the user saying they haven't
-        // signed up for anything yet.
-        boolean showed = false;
-
-        // If we have any AutomobileInsurance objects. . .
-        if (!autos.isEmpty()){
-            // Display each of them.
-            for (AutomobileInsurance insurance : autos){
-                insurance.display();
-            }
-            showed = true;
-        }
-
-        // If we have any PropertyInsurance objects. . .
-        if (!properties.isEmpty()){
-            // Display each of them
-            for (PropertyInsurance insurance : properties){
-                insurance.display();
-            }
-            showed = true;
-        }
-
-        // You get the idea
-        if (!travel.isEmpty()){
-
-            for (TravelInsurance insurance : travel){
-                insurance.display();
-            }
-            showed = true;
-        }
-
-        // Show the user that they are not enrolled in. . .well, jesus, look at the print argument!
-        if (!showed){
-            BuyInsurance.print("You haven't enrolled in any insurance yet!");
-        } else {
-            BuyInsurance.print(String.format("Total Premium:\t$%.2f", InsuranceAgentApp.getTotalPremium()));
-        }
-    }
-}
